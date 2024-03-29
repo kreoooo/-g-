095f282e9e44e71b4d78bb9902dcfa0782b3942e
kreoooo <129646630+kreoooo@users.noreply.github.com>
2024-03-29 20:00:41 +0100

    Update package-search-index.js

--- docs-server/package-search-index.js
+++ docs-server/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"pl.kielce.tu.battleshipsserverv4","l":"pl.kielce.tu.battleshipsserverv4"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"m":"pl.kato.tu.battleshipsserverv4","l":"pl.kato.tu.battleshipsserverv4"}];updateSearchResults();
